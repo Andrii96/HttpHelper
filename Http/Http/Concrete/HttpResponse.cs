28f9b881cf3849332cd413adee5269a39d408ecd
Andrew Popilevych <apopilevych.coi@gmail.com>
2018-05-04 11:12:01 +0300

    Changes

---
+++ Http/Http/Concrete/HttpResponse.cs
@@ -0,0 +1,50 @@
+﻿
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpHelper
+{
+    public class HttpResponse : IHttpResponse
+    {
+        #region Fields
+        private HttpResponseMessage _responseMessage;
+        #endregion
+
+        #region Constructor
+        public HttpResponse(HttpResponseMessage response)
+        {
+            _responseMessage = response;
+        }
+        #endregion
+
+        #region Properties
+
+        public HttpStatusCode StatusCode => _responseMessage.StatusCode;
+
+        public bool IsSuccess => _responseMessage.IsSuccessStatusCode;
+
+        #endregion
+
+        #region Methods
+
+        public async Task<T> DeserializeContent<T>()
+        {
+            var stringResult = await _responseMessage.Content.ReadAsStringAsync();
+            var deserializedResult = JsonConvert.DeserializeObject<T>(stringResult);
+            return deserializedResult;
+        }
+
+        public async Task<string> GetErrorDescription()
+        {
+            return _responseMessage.IsSuccessStatusCode ? null : await _responseMessage.Content.ReadAsStringAsync();
+        }
+
+        #endregion
+
+    }
+}
---
+++ Http/Http/Concrete/RequestBuilder.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+
+namespace HttpHelper
+{
+    public class RequestBuilder : IRequestBuilder
+    {
+        #region Properties
+        public string Url { get; private set; }
+        public MethodType MethodType { get; private set; }
+        public object Content { get; private set; }
+        #endregion
+
+        #region Constructor
+        private RequestBuilder()
+        {
+            MethodType = MethodType.None;
+        }
+        #endregion
+
+        #region Methods
+
+        public static IRequestBuilder CreateRequest()
+        {
+            return new RequestBuilder();
+        }
+
+        public IRequestBuilder UseMethod(MethodType methodType)
+        {
+            MethodType = methodType;
+            return this;
+        }
+
+        public IRequestBuilder UseUrl(string url)
+        {
+            Url = url;
+            return this;
+        }
+
+        public IRequestBuilder RequestBody(object content)
+        {
+            Content = content;
+            return this;
+        }
+
+        public IHttpRequest Build()
+        {
+            if (MethodType == MethodType.None || string.IsNullOrEmpty(Url))
+            {
+                throw new InvalidOperationException("Url can not be null or empty and method type should be selected.");
+            }
+
+            return new HttpRequest(this);
+        }
+
+        #endregion
+
+    }
+}
